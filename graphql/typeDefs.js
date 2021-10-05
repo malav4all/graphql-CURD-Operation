e2f5b3ee929e3a46d02b291e07f2dcc9f33554f8
Malav Naagar <73930050+malav4all@users.noreply.github.com>
2021-10-05 15:37:41 +0530

    Add files via upload

---
+++ graphql/typeDefs.js
@@ -0,0 +1,26 @@
+const {gql} = require('apollo-server-express')
+const typeDefs = gql `
+type Post {
+    id:ID
+    name:String
+    email:String
+    phone:String
+}
+ type Query {
+     getAllPost:[Post]
+     getPost(id:ID):Post
+ }
+
+ input PostInput {
+     name: String
+     email: String
+     phone: String
+ }
+ type Mutation {
+     createPost(post: PostInput): Post
+     deletePost(id:ID): String
+     updatePost(id:ID,post: PostInput): Post
+ }
+`
+
+module.exports = typeDefs
